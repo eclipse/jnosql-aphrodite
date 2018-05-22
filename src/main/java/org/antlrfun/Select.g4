1e474dd72b83ac031daed9577c678ebb1f82343f
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-22 11:44:21 -0300

    fixes

--- src/main/java/org/antlrfun/Select.g4
+++ src/main/java/org/antlrfun/Select.g4
@@ -4,8 +4,6 @@ r   : 'select' fields 'from' entity;
 
 fields: star | name (',' name)*;
 star: '*';
-order: 'order by' | order_clause (' ' order_clause)*;
-order_clause: name | name 'asc' | name 'desc';
 name: ANY_NAME;
 entity: ANY_NAME;
 ANY_NAME: [a-zA-Z_.] [a-zA-Z._0-9]*;
--- src/test/resources/jnosql.sql
+++ src/test/resources/jnosql.sql
@@ -3,9 +3,3 @@ select name,age from Person
 select  name,age,adress.age from Person
 select name , age from Person
 select  name, age ,adress.age from Person
-select  name, age ,adress.age from Person order by name
-select  name, age ,adress.age from Person order by name asc
-select  name, age ,adress.age from Person order by name desc
-select  name, age ,adress.age from Person order by name asc age asc
-select  name, age ,adress.age from Person order by name desc age
-select  name, age ,adress.age from Person order by name desc age desc
