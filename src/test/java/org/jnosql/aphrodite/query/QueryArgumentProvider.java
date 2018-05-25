5223dab0a005fca637e5f083bb69a27a097b293f
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-25 10:38:54 -0300

    removes constants

--- src/test/java/org/jnosql/aphrodite/query/QueryArgumentProvider.java
+++ src/test/java/org/jnosql/aphrodite/query/QueryArgumentProvider.java
@@ -12,11 +12,7 @@
 
 package org.jnosql.aphrodite.query;
 
-import java.util.function.Predicate;
-
 class QueryArgumentProvider extends AbstractArgumentProvider {
-
-    private static final Predicate<String> IS_COMMENT = s -> s.startsWith("#");
 
     @Override
     protected String getFile() {
