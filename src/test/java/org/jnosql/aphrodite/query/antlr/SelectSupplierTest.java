cec76ac352f37a320eed761bebf50152d0537fe7
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-26 19:08:34 -0300

    fixes tests

--- src/test/java/org/jnosql/aphrodite/query/antlr/SelectSupplierTest.java
+++ src/test/java/org/jnosql/aphrodite/query/antlr/SelectSupplierTest.java
@@ -16,7 +16,6 @@ import org.jnosql.aphrodite.query.SelectQuery;
 import org.jnosql.aphrodite.query.SelectSupplier;
 import org.jnosql.aphrodite.query.Sort;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -24,6 +23,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.jnosql.aphrodite.query.Sort.SortType.ASC;
+import static org.jnosql.aphrodite.query.Sort.SortType.DESC;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -39,8 +40,7 @@ class SelectSupplierTest {
         });
     }
 
-    @ParameterizedTest
-    @DisplayName("Should return the query 'select * from God'")
+    @ParameterizedTest(name = "Should return the query {0}")
     @ValueSource(strings = { "select * from God" })
     public void shouldReturnParserQuery(String query) {
         SelectQuery selectQuery = selectSupplier.apply(query);
@@ -52,8 +52,7 @@ class SelectSupplierTest {
         assertFalse(selectQuery.getWhere().isPresent());
     }
 
-    @ParameterizedTest
-    @DisplayName("Should return the query 'select name, address from God'")
+    @ParameterizedTest(name = "Should return the query {0}")
     @ValueSource(strings = { "select name, address from God" })
     public void shouldReturnParserQuery2(String query) {
         SelectQuery selectQuery = selectSupplier.apply(query);
@@ -66,8 +65,7 @@ class SelectSupplierTest {
         assertFalse(selectQuery.getWhere().isPresent());
     }
 
-    @ParameterizedTest
-    @DisplayName("Should return the query 'select name, address from God order by name order by name'")
+    @ParameterizedTest(name = "Should return the query {0}")
     @ValueSource(strings = { "select name, address from God order by name" })
     public void shouldReturnParserQuery3(String query) {
         SelectQuery selectQuery = selectSupplier.apply(query);
@@ -76,14 +74,40 @@ class SelectSupplierTest {
         assertThat(selectQuery.getFields(), contains("name", "address"));
         assertFalse(selectQuery.getOrderBy().isEmpty());
         assertThat(selectQuery.getOrderBy().stream().map(Sort::getName).collect(toList()), contains("name"));
-        assertThat(selectQuery.getOrderBy().stream().map(Sort::getType).collect(toList()), contains(Sort.SortType.ASC));
+        assertThat(selectQuery.getOrderBy().stream().map(Sort::getType).collect(toList()), contains(ASC));
         assertEquals(0, selectQuery.getLimit());
         assertEquals(0, selectQuery.getSkip());
         assertFalse(selectQuery.getWhere().isPresent());
     }
-    /*select  name, age ,adress.age from Person order by name
-    select  name, age ,adress.age from Person order by name asc
-    select  name, age ,adress.age from Person order by name desc
-    select  name, age ,adress.age from Person order by name asc age asc*/
+
+    @ParameterizedTest(name = "Should return the query {0}")
+    @ValueSource(strings = { "select name, address from God order by name desc" })
+    public void shouldReturnParserQuery4(String query) {
+        SelectQuery selectQuery = selectSupplier.apply(query);
+        assertEquals("God", selectQuery.getEntity());
+        assertFalse(selectQuery.getFields().isEmpty());
+        assertThat(selectQuery.getFields(), contains("name", "address"));
+        assertFalse(selectQuery.getOrderBy().isEmpty());
+        assertThat(selectQuery.getOrderBy().stream().map(Sort::getName).collect(toList()), contains("name"));
+        assertThat(selectQuery.getOrderBy().stream().map(Sort::getType).collect(toList()), contains(DESC));
+        assertEquals(0, selectQuery.getLimit());
+        assertEquals(0, selectQuery.getSkip());
+        assertFalse(selectQuery.getWhere().isPresent());
+    }
+
+    @ParameterizedTest(name = "Should return the query {0}")
+    @ValueSource(strings = { "select name, address from God order by name desc age asc" })
+    public void shouldReturnParserQuery5(String query) {
+        SelectQuery selectQuery = selectSupplier.apply(query);
+        assertEquals("God", selectQuery.getEntity());
+        assertFalse(selectQuery.getFields().isEmpty());
+        assertThat(selectQuery.getFields(), contains("name", "address"));
+        assertFalse(selectQuery.getOrderBy().isEmpty());
+        assertThat(selectQuery.getOrderBy().stream().map(Sort::getName).collect(toList()), contains("name", "age"));
+        assertThat(selectQuery.getOrderBy().stream().map(Sort::getType).collect(toList()), contains(DESC, ASC));
+        assertEquals(0, selectQuery.getLimit());
+        assertEquals(0, selectQuery.getSkip());
+        assertFalse(selectQuery.getWhere().isPresent());
+    }
 
 }
