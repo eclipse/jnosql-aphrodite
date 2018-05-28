f8bc6d4c61e480dc168730376ea1408ccbfe6e57
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-28 09:07:07 -0300

    defines default insert query

--- src/main/java/org/jnosql/aphrodite/antlr/DefaultInsertSupplier.java
+++ src/main/java/org/jnosql/aphrodite/antlr/DefaultInsertSupplier.java
@@ -15,17 +15,22 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.jnosql.query.Condition;
 import org.jnosql.query.InsertQuery;
 import org.jnosql.query.InsertSupplier;
+import org.jnosql.query.Operator;
+import org.jnosql.query.Value;
 
 import java.time.Duration;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 class DefaultInsertSupplier extends AbstractSupplier implements InsertSupplier {
 
     private String entity;
 
-    private List<Condition> conditions = new ArrayList<>();
+    private List<Condition> conditions = Collections.emptyList();
 
     private Duration duration;
 
@@ -41,13 +46,19 @@ class DefaultInsertSupplier extends AbstractSupplier implements InsertSupplier {
 
     @Override
     public void exitChanges(QueryParser.ChangesContext ctx) {
-        ctx.
+        this.conditions = ctx.change().stream().map(this::getCondition).collect(toList());
+    }
+
+    private Condition getCondition(QueryParser.ChangeContext changeContext) {
+        String name = changeContext.name().getText();
+        Value<?> value = ValueConverter.get(changeContext.value());
+        return new DefaultCondition(name, Operator.EQUALS, value);
     }
 
 
     @Override
     public InsertQuery apply(String query) {
         runQuery(query);
-        return null;
+        return new DefaultInsertQuery(entity, duration, conditions);
     }
 }
