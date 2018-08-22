06cee265c2c9e1c411cfe3de5e339266d1351a97
Otavio Santana <otaviopolianasantana@gmail.com>
2018-08-22 19:09:13 -0300

    adds Cachequery test

---
+++ jnosql-query-antlr/src/test/java/org/jnosql/aphrodite/antlr/cache/CacheQueryTest.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (c) 2018 Otávio Santana and others
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *  You may elect to redistribute this code under either of these licenses.
+ *  Contributors:
+ *  Otavio Santana
+ */
+package org.jnosql.aphrodite.antlr.cache;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CacheQueryTest {
+
+    private CacheQuery<Integer> cacheQuery;
+
+    @BeforeEach
+    public void setUp() {
+        cacheQuery = new CacheQuery<>(Integer::parseInt);
+    }
+
+
+    @Test
+    public void shouldPut() {
+
+        int total = 1_000_000;
+        for (int index = 0; index < total; index++) {
+            Integer value = cacheQuery.get(Integer.toString(index));
+            assertNotNull(value);
+            assertEquals(Integer.valueOf(index), value);
+        }
+        assertTrue(cacheQuery.size() < total);
+    }
+}
