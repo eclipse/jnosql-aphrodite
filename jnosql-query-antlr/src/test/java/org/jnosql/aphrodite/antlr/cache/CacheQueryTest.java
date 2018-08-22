e842c0370a87de89026cb9809f4c66bb17cb06f4
Otavio Santana <otaviopolianasantana@gmail.com>
2018-08-22 19:24:01 -0300

    fixes import

--- jnosql-query-antlr/src/test/java/org/jnosql/aphrodite/antlr/cache/CacheQueryTest.java
+++ jnosql-query-antlr/src/test/java/org/jnosql/aphrodite/antlr/cache/CacheQueryTest.java
@@ -11,11 +11,12 @@
  */
 package org.jnosql.aphrodite.antlr.cache;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CacheQueryTest {
 
